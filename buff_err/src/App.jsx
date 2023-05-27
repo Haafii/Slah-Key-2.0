9abb84ba6948802309a09e238313c9efa4fe1c27
Haafii <hafismuhammedtk@gmail.com>
2023-05-27 15:54:06 +0530

    login reg page added

--- buff_err/src/App.jsx
+++ buff_err/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
-
   return (
     <div>
-      Hello
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
